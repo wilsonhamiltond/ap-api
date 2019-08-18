57fdaea375fc322481cf38e97b14756735060a0c
wilson hamilton <whamilton@simetricaconsulting.com>
2019-08-18 11:02:59 -0400

    Refactoring codes

--- src/routers/author.router.ts
+++ src/routers/author.router.ts
@@ -5,104 +5,53 @@ export class AuthorRouter {
     }
     static create(event, context, callback) {
         new AuthorService().create(event, (error, result) => {
-            const response = {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({
-                    result: true,
-                    data: result
-                })
-            };
-
-            context.succeed(response);
+            AuthorRouter.respose(context, error,result);
         });
     }
 
     static list(event, context, callback) {
         new AuthorService().list(event, (error, result) => {
-            const response = {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({
-                    result: true,
-                    data: result
-                })
-            };
-
-            context.succeed(response);
+            AuthorRouter.respose(context, error,result);
         });
     }
 
     static get(event, context, callback) {
         new AuthorService().get(event, (error, result) => {
-            const response = {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({
-                    result: true,
-                    data: result
-                })
-            };
-
-            context.succeed(response);
+            AuthorRouter.respose(context, error,result);
         });
     }
 
     static update (event, context, callback){
         new AuthorService().update(event, (error, result) => {
-            const response = {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({
-                    result: true,
-                    data: result
-                })
-            };
-
-            context.succeed(response);
+            AuthorRouter.respose(context, error,result);
         });
     }
 
     static delete (event, context, callback) {
         new AuthorService().delete(event, (error, result) => {
-            const response = {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({
-                    result: true,
-                    data: result
-                })
-            };
-
-            context.succeed(response);
+            AuthorRouter.respose(context, error,result);
         });
     }
     
     static filter (event, context, callback) {
         new AuthorService().filter(event, (error, result) => {
-            const response = {
-                statusCode: 200,
-                headers: {
-                    "Access-Control-Allow-Origin": "*"
-                },
-                body: JSON.stringify({
-                    result: !error,
-                    data: result,
-                    message: error
-                })
-            };
-
-            context.succeed(response);
+            AuthorRouter.respose(context, error,result);
        });
     }
+    
+    static respose(context:any , error:any, result: any){
+        const response = {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*"
+            },
+            body: JSON.stringify({
+                result: !error,
+                data: result,
+                message: error
+            })
+        };
+
+        context.succeed(response);
+   }
 }
